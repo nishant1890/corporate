8d71a49f0543b90adc178ad0011ea8f906648573
Nishant Goel <goelnishant26@gmail.com>
2017-02-21 01:43:25 +0530

    revenue api fixes

--- app/controllers/api/v1/revenues_controller.rb
+++ app/controllers/api/v1/revenues_controller.rb
@@ -1,17 +1,24 @@
 class Api::V1::RevenuesController < Api::V1::BaseController
 
   def create
-    company = Company.find_by(name: params["revenue"]["company_name"])
-    
-    if company.present?
-      revenue = company.revenues.build(revenue_params)
-      if revenue.save
-        render json: {revenue: revenue.attributes}, status: 200
-      else
-        render json: {error: "Failed to save the revenue", errors: formatted_error_messages(revenue)}, status: 422
+    if params[:revenues].try(:values).present?
+      create_status = params[:revenues].each_with_object({}) do |(index, object), h|
+        company = Company.find_by(name: object[:company_name])
+        
+        if company.present?
+          revenue = company.revenues.build(object)
+          if revenue.save
+            h[index] = {revenue: revenue.attributes}
+          else
+            h[index] = {error: "Failed to save the revenue", errors: formatted_error_messages(revenue)}
+          end
+        else
+          h[index] = {error: "Failed to save the revenue", errors: {company: 'Invalid company'}}
+        end
       end
+      render json: create_status, status: 200
     else
-      render json: {error: "Failed to save the revenue", errors: {company: 'Invalid company'}}, status: 422
+      render json: {error: "Failed to save revenues", errors: {revenue: "Please send atleast one revenue object"}}
     end
   end
 
